05508ea5845fc1df66b08bdadcb15a7604898fbe
Thanaporn019 <joon@hotmail.com>
2021-03-02 15:35:57 +0700

    uppppppppppppppppp sever fe

--- tsm-fe/frontend.js
+++ tsm-fe/frontend.js
@@ -2,9 +2,22 @@ const express = require('express');
 const app = express();
 const fs = require("fs");
 
-const frontEndPort = process.env.FE_PORT || 5432;
+const frontEndPort = process.env.FE_PORT || 5001;
 
-app.use(express.static( __dirname + '/build'));
+app.use(express.static( __dirname + '/dist'));
+
+/*
+const https = require('https');
+const key = fs.readFileSync(__dirname + '/ssl/bluefin-portal-key.pem');
+const cert = fs.readFileSync(__dirname + '/ssl/bluefin-portal-cert.pem');
+const options = {
+    key: key,
+    cert: cert
+};
+*/
+console.log('frontEndPort : ' + frontEndPort);
+//https.createServer(options, app).listen(frontEndPort);
+
 
 app.listen(frontEndPort, function () {
     console.log('frontEndPort app listening on port ' + frontEndPort + '!');
